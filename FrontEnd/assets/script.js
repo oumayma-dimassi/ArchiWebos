366e6899c9466ac65e290f13b09c80002004d47c
unknown <zarradoumayma@gmail.com>
2024-11-08 13:03:50 +0100

    filtre categories

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,119 +1,59 @@
+let ApiWorksResponse = await fetch('http://localhost:5678/api/works');
+let allWorks = await ApiWorksResponse.json();
+
 const gallery = document.getElementById("gallery");
+displayWorks(allWorks);
 
-function getWorks() {
-
-fetch('http://localhost:5678/api/works')
-  .then(response => response.json())
-  .then(data => {
-
-    for (let i = 0; i < data.length; i++) {
-
-      const figure = document.createElement("figure");
-      const image = document.createElement("img");
-      const figcaption = document.createElement("figcaption");
-
-      figcaption.innerHTML = data[i].title;
-      image.src = data[i].imageUrl;
-      image.alt = data[i].title;
-
-      figure.appendChild(image);
-      figure.appendChild(figcaption);
-      gallery.appendChild(figure);
-    }
-    
-  });
-}
-
-getWorks();
-
-
-const categories = document.getElementById("categories");
-function getcategories(){
-
-  fetch('http://localhost:5678/api/categories')
-  .then(response => response.json())
-  .then(data => {
-  for(let i = 0; i < data.length; i++){
-    const button = document.createElement("button");
-    button.id = data[i].id;
-    button.innerHTML = data[i].name;
-    button.className = "categorie";
-    categories.appendChild(button);
-    button.addEventListener('click', getCategoriesByID, false);
-   }
- });
-}
-
-getcategories();
-
-const categorie = document.getElementsByClassName("categorie");
-
-var getCategoriesByID = function(evt) {
-
-  const myID = evt.currentTarget.id;
+function displayWorks(works) {
 
   gallery.replaceChildren();
 
-  fetch('http://localhost:5678/api/works')
-  .then(response => response.json())
-  .then(data => {
-
-    for (let i = 0; i < data.length; i++) {
-
-      if(myID == data[i].categoryId || myID == 0){
+  for (let i = 0; i < works.length; i++) {
         const figure = document.createElement("figure");
         const image = document.createElement("img");
         const figcaption = document.createElement("figcaption");
   
-        figcaption.innerHTML = data[i].title;
-        image.src = data[i].imageUrl;
-        image.alt = data[i].title;
+        figcaption.innerHTML = works[i].title;
+        image.src = works[i].imageUrl;
+        image.alt = works[i].title;
   
         figure.appendChild(image);
         figure.appendChild(figcaption);
         gallery.appendChild(figure);
-      }
     }
-    
-  });
+}
 
-};
+let categories = document.getElementById("categories");
 
+function getcategories(){
+  fetch('http://localhost:5678/api/categories')
+  .then(response => response.json())
+  .then(data => {
 
-categorie[0].addEventListener('click', getCategoriesByID, false);
+      for(let i = 0; i < data.length; i++){
+        const button = document.createElement("button");
+        button.id = data[i].id;
+        button.innerHTML = data[i].name;
+        button.className = "category";
+        categories.appendChild(button);
+      }
 
+       const allCategories = document.querySelectorAll(".category");
+       allCategories.forEach(category =>{
 
+        category.addEventListener('click', () => {
+        allCategories.forEach(selected => selected.classList.remove("selectedCategory"))
+        category.classList.add("selectedCategory");
+            const categoryId = category.id;
+            if(categoryId === '0'){
+              displayWorks(allWorks);
+            }else{
+              const filtredWorks = allWorks.filter(work => work.categoryId == categoryId);
+              displayWorks(filtredWorks);
+            }
+        })
+       });
+    });
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//const categories 
-//  function getcategories
-// fetch categories
-// les deux then
-//boucle for 
-//const button createlement boutton
-//class categorie
-//id+valeur de LAPI
-//name categorie dans LAPI 
+getcategories();
